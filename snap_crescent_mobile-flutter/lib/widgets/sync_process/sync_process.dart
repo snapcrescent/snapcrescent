e646c8a631ea600bcfe1d4b7715682adaeea74bb
Naval Gandhi <navalgandhi1989@gmail.com>
2022-12-06 22:08:12 -0500

    Some optimizations in sync process

--- snap_crescent_mobile-flutter/lib/widgets/sync_process/sync_process.dart
+++ snap_crescent_mobile-flutter/lib/widgets/sync_process/sync_process.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
@@ -13,6 +15,7 @@ class SyncProcessWidget extends StatefulWidget {
 
 class SyncProcessWidgetState extends State<SyncProcessWidget> {
   late SyncProcessStore _syncProcessStore;
+  Timer? timer;
 
   @override
   void initState() {
@@ -20,11 +23,19 @@ class SyncProcessWidgetState extends State<SyncProcessWidget> {
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _syncProcessStore.startSyncProcess();
+      timer = Timer.periodic(Duration(seconds: 30),
+          (Timer t) => _syncProcessStore.startSyncProcess());
     });
   }
 
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
   _getPercentage(int? count, int? total) {
-      return (count! * 100/total!).toStringAsFixed(2);
+    return (count! * 100 / total!).toStringAsFixed(2);
   }
 
   _syncProgress(SyncProgress _syncProgressState) {
@@ -32,13 +43,15 @@ class SyncProcessWidgetState extends State<SyncProcessWidget> {
 
     if (_syncProgressState == SyncProgress.DOWNLOADING &&
         _syncProcessStore.totalServerAssetCount != 0) {
-      progressLabel = '''Downloading (${_getPercentage(_syncProcessStore.downloadedAssetCount, _syncProcessStore.totalServerAssetCount)}%)''';
+      progressLabel =
+          '''Downloading (${_getPercentage(_syncProcessStore.downloadedAssetCount, _syncProcessStore.totalServerAssetCount)}%)''';
     } else if (_syncProgressState == SyncProgress.UPLOADING &&
         _syncProcessStore.totalLocalAssetCount != 0) {
       progressLabel =
           '''Uploading (${_getPercentage(_syncProcessStore.uploadedAssetCount, _syncProcessStore.totalLocalAssetCount)}%)''';
+    } else if (_syncProgressState == SyncProgress.PROCESSING) {
+      
     } else {
-      progressLabel = "";
       return Container();
     }
 
@@ -46,28 +59,14 @@ class SyncProcessWidgetState extends State<SyncProcessWidget> {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        if (progressLabel.isNotEmpty)
+          Text(progressLabel,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              )),
         Container(
-            child: Row(children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: new Text(progressLabel,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                )),
-          ),
-          Expanded(
-            flex: 1,
-            child: IconButton(
-                    onPressed: () {
-                      _syncProcessStore.cancelSync();
-                    },
-                    alignment:Alignment.topRight,
-                    icon: Icon(Icons.cancel_rounded, color: Colors.white,)),
-          ),
-        ])),
-        Container(
-          height: 1,
+          height: 2,
           child: const LinearProgressIndicator(),
         ),
       ],
@@ -77,14 +76,12 @@ class SyncProcessWidgetState extends State<SyncProcessWidget> {
   @override
   Widget build(BuildContext context) {
     _syncProcessStore = Provider.of<SyncProcessStore>(context);
-    _syncProcessStore.assetStore =  Provider.of<AssetStore>(context);
+    _syncProcessStore.assetStore = Provider.of<AssetStore>(context);
 
-   
     return Observer(
         builder: (context) => _syncProcessStore.syncProgressState !=
                 SyncProgress.CONTACTING_SERVER
             ? _syncProgress(_syncProcessStore.syncProgressState)
             : LinearProgressIndicator());
-    
   }
 }
