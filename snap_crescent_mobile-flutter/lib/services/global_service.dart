4608d2c9d076b60610ed6e480f9de29354023e6d
navalgandhi1989 <navalgandhi1989@gmail>
2023-01-14 13:33:16 -0500

    Fixed sync process bug for skipping some assets

--- snap_crescent_mobile-flutter/lib/services/global_service.dart
+++ snap_crescent_mobile-flutter/lib/services/global_service.dart
@@ -1,7 +1,14 @@
 
+import 'package:snap_crescent/stores/widget/sync_process_store.dart';
+
 class GlobalService {
 
   GlobalService._privateConstructor():super();
   static final GlobalService instance = GlobalService._privateConstructor();
+
+  SyncProcessStore? syncProcessStore;
+
+  
+
 
 }
