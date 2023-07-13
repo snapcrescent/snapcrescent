cfdb1661e06fa886aae9b7668b50ef1eae5fde65
Naval Gandhi <navalgandhi1989@gmail.com>
2023-07-13 14:41:52 -0400

    Removed DB name from liquibase files

--- snapcrescent/src/main/resources/db/changelog/tables/album_asset_assn.sql
+++ snapcrescent/src/main/resources/db/changelog/tables/album_asset_assn.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `album_asset_assn` (
   `album_id` BIGINT NOT NULL,
   `asset_id` BIGINT NOT NULL,
   PRIMARY KEY (`album_id`, `asset_id`) ,
-  CONSTRAINT `fk_album_asset_assn_album_id` FOREIGN KEY (`album_id`) REFERENCES `snap_crescent`.`album`(`id`),
-  CONSTRAINT `fk_album_asset_assn_asset_id` FOREIGN KEY (`asset_id`) REFERENCES `snap_crescent`.`asset`(`id`)
+  CONSTRAINT `fk_album_asset_assn_album_id` FOREIGN KEY (`album_id`) REFERENCES `album`(`id`),
+  CONSTRAINT `fk_album_asset_assn_asset_id` FOREIGN KEY (`asset_id`) REFERENCES `asset`(`id`)
 );
 -- rollback DROP TABLE if exists album_asset_assn;
--- snapcrescent/src/main/resources/db/changelog/tables/album_user_assn.sql
+++ snapcrescent/src/main/resources/db/changelog/tables/album_user_assn.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `album_user_assn` (
   `album_id` BIGINT NOT NULL,
   `user_id` BIGINT NOT NULL,
   PRIMARY KEY (`album_id`, `user_id`) ,
-  CONSTRAINT `fk_album_user_assn_album_id` FOREIGN KEY (`album_id`) REFERENCES `snap_crescent`.`album`(`id`),
-  CONSTRAINT `fk_album_user_assn_user_id` FOREIGN KEY (`user_id`) REFERENCES `snap_crescent`.`user`(`id`)
+  CONSTRAINT `fk_album_user_assn_album_id` FOREIGN KEY (`album_id`) REFERENCES `album`(`id`),
+  CONSTRAINT `fk_album_user_assn_user_id` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
 );
 -- rollback DROP TABLE if exists album_user_assn;
